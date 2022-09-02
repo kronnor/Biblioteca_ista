a92f4a7b2f2cd5d1036f8c15403bb1927e4a5484
Kevin Landivar <kevin.landivar.est@tecazuay.edu.ec>
2022-09-02 18:02:41 -0500

    Proyecto

---
+++ app/src/main/java/com/example/biblioteca_f_ciclo/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.biblioteca_f_ciclo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
