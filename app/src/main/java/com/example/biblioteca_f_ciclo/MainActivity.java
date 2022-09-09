0acd5cb3bb6c4496484f676262e671d60de37e85
Kevin Landivar <kevin.landivar.est@tecazuay.edu.ec>
2022-09-09 18:41:09 -0500

    barra de navegacion

--- app/src/main/java/com/example/biblioteca_f_ciclo/MainActivity.java
+++ app/src/main/java/com/example/biblioteca_f_ciclo/MainActivity.java
@@ -2,13 +2,27 @@ package com.example.biblioteca_f_ciclo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+
+import com.example.biblioteca_f_ciclo.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        ActivityMainBinding binding=ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.button.setOnClickListener(v -> {
+            openActivity();
+        });
+    }
+
+    private  void openActivity(){
+        Intent intent = new Intent(this,MainActivity2.class);
+        startActivity(intent);
     }
 }
